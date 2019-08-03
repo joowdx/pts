efe6029764d4f39609e5448c187822a4355ce2d6
joowdx <joowdx@pm.me>
2019-08-03 13:12:22 +0800

    fixed timestamps

--- database/migrations/0000_00_00_000000_create_application_table.php
+++ database/migrations/0000_00_00_000000_create_application_table.php
@@ -18,8 +18,7 @@ class CreateApplicationTable extends Migration
             $table->bigIncrements('id');
             $table->string('key', 20)->unique();
             $table->text('value');
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamps();
         });
         Application::create([
             'key' => 'APP NAME',
